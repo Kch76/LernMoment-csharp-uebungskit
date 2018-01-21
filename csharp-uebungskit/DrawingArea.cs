d0757f2f088f6227df406673c72f49e39da4a78b
gustda <d.gustorff@gmail.com>
2018-01-21 15:20:01 +0100

    Initialer Checkin

---
+++ csharp-uebungskit/DrawingArea.cs
@@ -0,0 +1,34 @@
+﻿using csharp_canvas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_uebungskit
+{
+    class DrawingArea
+    {
+        CsharpCanvas canvas;
+        public DrawingArea()
+        {
+            // erzeuge ein neues Canvas
+            canvas = new CsharpCanvas();
+            
+            // Vorder- und Hintergrund definieren
+            canvas.SetBackgroundColor(0x00, 0xA8, 0xC6);
+            canvas.SetForegroundColor(System.Drawing.Color.Blue);
+
+            // Draw Event abonieren
+            canvas.Draw += Canvas_Draw;
+
+            // Initialisieren für kontinuierliches Zeichnen mit 30 Frames
+            canvas.Setup(30);        
+        }
+
+        private void Canvas_Draw()
+        {
+            // Hier können Elemente gezeichnet werden
+        }        
+    }
+}
